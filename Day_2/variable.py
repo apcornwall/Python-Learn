0a89b5ec8610378eb1bdfff9edb7d6e52f4da475
Farhadi <ahmadfarhadi944@gmail.com>
2026-07-21 20:31:57 +0700

    Add files via upload

---
+++ Day_2/variable.py
@@ -0,0 +1,14 @@
+# Day 2:30 Days of Python Programming
+
+first_name = "Suep"
+last_name = "Mujaer"
+full_name = "Suep Mujaer"
+country = "Indonesia"
+city = "Solo"
+age = 3
+year = 2026
+is_married = False
+is_True = True
+is_light_on = True
+
+race, fur_color, is_male = "cat", "gray", True
